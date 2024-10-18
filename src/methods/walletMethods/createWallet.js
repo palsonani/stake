8eb26c58d159e8e232e293510bd0ff580b9902aa
palsonani <palsonani02@gmail.com>
2024-10-18 17:07:15 +0530

    add create/get wallet api

--- src/methods/walletMethods/createWallet.js
+++ src/methods/walletMethods/createWallet.js
@@ -3,12 +3,18 @@ import Wallet from '../../models/Wallet.js';
 import { addLog } from '../../utility/logs.js';
 
 export const createWallet = async (req, res) => {
-    const { userId } = req.body;
+    const userId = req.params.userId;
 
     if (!userId) {
-        return res.status(400).json({ message: 'userId is required' });
+        return res.status(400).json({ message: 'userId is required!' });
     }
     const user = await User.findByPk(userId);
+    const wallet = await Wallet.findByPk(userId);
+
+    if (wallet) {
+        return res.status(400).json({ message: 'wallet alrady exist!' });
+    }
+
     try {
         const newWallet = await Wallet.create({
             userId,
@@ -30,7 +36,7 @@ export const createWallet = async (req, res) => {
     } catch (error) {
         // Log the error in case of failure
         await addLog({
-            userId: null,
+            userId: userId,
             userName: user.userName,
             actionType: 'ERROR',
             actionDescription: `Error creating wallet`,
--- src/routes/walletRoutes.js
+++ src/routes/walletRoutes.js
@@ -2,11 +2,13 @@ import express from 'express';
 import { redeemCoupon } from '../methods/walletMethods/redeemCoupon.js';
 import { generateCoupon } from '../methods/walletMethods/generateCoupen.js';
 import { createWallet } from '../methods/walletMethods/createWallet.js';
+import { getWallet } from '../methods/walletMethods/find.js';
 
 const walletRoutes = express.Router();
 
 walletRoutes.post('/generate', generateCoupon);
 walletRoutes.post('/redeem', redeemCoupon)
-walletRoutes.post('/create', createWallet)
+walletRoutes.post('/create/:userId', createWallet)
+walletRoutes.get('/get/:userId', getWallet)
 
 export default walletRoutes;
